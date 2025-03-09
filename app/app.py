4d263a4d8da52515ce5cafe0a6b9033cb147f958
SemelaEV <semela.ev@gmail.com>
2025-03-09 19:11:59 +0300

    Add form, change methods in app

--- app/app.py
+++ app/app.py
@@ -1,39 +1,52 @@
 import uuid
+import cgi
+import os
+import json
 from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
+from os import listdir
+from os.path import isfile, join
+from PIL import Image
 from loguru import logger
 
 SERVER_ADDRESS = ('0.0.0.0', 8000)
 ALLOWED_EXTENSIONS = ('jpg', 'jpeg', 'png', 'gif')
 ALLOWED_LENGTH = (5 * 1024 * 1024)
+UPLOAD_DIR = 'images'
 
 logger.add('logs/app.log', format="[{time: YYYY-MM-DD HH:mm:ss}] | {level} | {message}")
 
 class ImageHostingHandler(BaseHTTPRequestHandler):
     server_version = 'Image Hosting Server/0.1'
 
-    routes = {
-            '/': 'get_index',
-            '/index.html': 'get_index',
-            '/upload': 'post_upload'
+    def __init__(self, request, client_address, server):
+        self.get_routes = {
+            '/upload': self.get_upload,
+            '/images': self.get_images,
         }
+        self.post_routes = {
+            '/upload': self.post_upload,
+        }
+        super().__init__(request, client_address, server)
 
-    def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
 
     def do_GET(self):
-        if self.path in self.routes:
-            exec(f'self.{self.routes[self.path]}()')
+        if self.path in self.get_routes:
+            self.get_routes[self.path]()
         else:
             logger.warning(f'GET 404 {self.path}')
             self.send_response(404, 'Not Found')
 
-    def get_index(self):
-        logger.info(f'GET {self.path}')
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(open('static/index.html', 'rb').read())
+
+    def do_POST(self):
+        if self.path in self.post_routes:
+            self.post_routes[self.path]()
+        else:
+            logger.warning(f'POST 404 {self.path}')
+            self.send_response(405, 'Method Not Allowed')
+
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
 
     def get_images(self):
         logger.info(f'GET {self.path}')
@@ -41,17 +54,15 @@ class ImageHostingHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json; charset=utf-8')
         self.end_headers()
 
-        #проверить откуда эти две строки
         images = [f for f in listdir(UPLOAD_DIR) if isfile(join('images', f))]
         self.wfile.write(json.dumps({'images': images}).encode('utf-8'))
 
-
-    def do_POST(self):
-        if self.path == '/upload':
-            exec(f'self.{self.routes[self.path]}()')
-        else:
-            logger.warning(f'POST 404 {self.path}')
-            self.send_response(405, 'Method Not Allowed')
+    def get_upload(self):
+        logger.info(f'GET {self.path}')
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(open('static/index.html', 'rb').read())
 
     def post_upload(self):
         logger.info(f'POST {self.path}')
@@ -61,27 +72,37 @@ class ImageHostingHandler(BaseHTTPRequestHandler):
             self.send_response(413, 'Payload Too large')
             return
 
-        filename = self.headers.get("Filename")
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST'}
+        )
 
-        if not filename:
-            logger.error("Lack of Filename header")
-            self.send_response(400, 'Lack of Filename header')
-            return
-        filename, ext = filename.split('.')
+        data = form['image'].file
+
+        _, ext = os.path.splitext(form['image'].filename)
+
         if ext not in ALLOWED_EXTENSIONS:
-            logger.error('Unsupported file extension')
-            self.send_response(400, 'Unsupported file extension')
+            logger.error("File type not allowed")
+            self.send_response(400, 'File type not allowed')
             return
 
-        data = self.rfile.read(content_length)
         image_id = uuid.uuid4()
+        image_name = f'{image_id}{ext}'
+        with open(f'{UPLOAD_DIR}/{image_name}', 'wb') as f:
+            f.write(data.read())
 
-        with open(f'images/{image_id}.{ext}', 'wb') as f:
-            f.write(data)
-        logger.info(f'Upload success: {image_id}.{ext}')
-        self.send_response(201)
-        self.send_header('Location', f'http://{SERVER_ADDRESS[0]}:{SERVER_ADDRESS[1]}/images/{filename}.{ext}')
-        self.send_header('Content-type', 'text/html; charset=utf-8')
+        try:
+            im = Image.open(f'{UPLOAD_DIR}/{image_name}')
+            im.verify()
+        except (IOError, SyntaxError) as e:
+            logger.error(f'Invalid file: {e}')
+            self.send_response(400, 'Invalid file')
+            return
+
+        logger.info(f'Upload success: {image_name}')
+        self.send_response(301)
+        self.send_header('Location', f'/images/{image_name}')
         self.end_headers()
         self.wfile.write(open('upload_success.html', 'rb').read())
 
